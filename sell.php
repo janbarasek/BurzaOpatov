42cc7a15c959bbefa275aa59b1e9d5482ed3473a
Patrik Holba <Paveegamedev@gmail.com>
2023-04-03 21:48:40 +0200

    add design for sell.php

--- sell.php
+++ sell.php
@@ -16,7 +16,7 @@
 </div>
 
 <img style='margin-left:5%;width:100px;margin-top:-80px;' src='Photos/books/2.jpg'></img>
-<h1 style='float:right;margin-right:10%;'>Vaše kniha</h1>
+<h1 style='float:right;margin-right:10%;'>Vaši knihu</h1>
 <hr style='padding:1px;margin-top:23px;margin-left:0px;width: 140px;background-color:black;'>
 <h3 style='font-size:18px;margin-left:150px;margin-top: -25px;'>Třída/Předmět</h3>
 <hr style='padding:1px;margin-top:-12px;float:right;width: 90px;background-color:black;'>
@@ -33,58 +33,62 @@ if(getClassByUserID($conn, $_SESSION['id'])['classYear'] == 4){
         </div>';
 }else if (getClassByUserID($conn, $_SESSION['id'])['classYear'] == 5){
     echo '
-
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="">Choose a year..</option>
-
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="4"><label> 4.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="5" checked><label> 5.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="6"><label> 6.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="7"><label> 7.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="8"><label> 8.  </label></input>';
+        <div class="yearSellContainer">
+        <input class="yearSell" type="radio" name="year" value="4" id="year4"><label for="year4" class="yearSellLabel"> Kvarta </label></input>
+        <input class="yearSell" type="radio" name="year" value="5" id="year5" checked><label for="year5" class="yearSellLabel"> Kvinta </label></input>
+        <input class="yearSell" type="radio" name="year" value="6" id="year6"><label for="year6" class="yearSellLabel"> Sexta  </label></input>
+        <input class="yearSell" type="radio" name="year" value="7" id="year7"><label for="year7" class="yearSellLabel"> Septima  </label></input>
+        <input class="yearSell" type="radio" name="year" value="8" id="year8"><label for="year8" class="yearSellLabel"> Oktáva  </label></input>
+        </div>';
 } else if (getClassByUserID($conn, $_SESSION['id'])['classYear'] == 6){
     echo '
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="">Choose a year..</option>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="4"><label> 4.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="5"><label> 5.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="6" checked><label> 6.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="7"><label> 7. </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="8"><label> 8. </label></input>';
+        <div class="yearSellContainer">
+        <input class="yearSell" type="radio" name="year" value="4" id="year4"><label for="year4" class="yearSellLabel"> Kvarta </label></input>
+        <input class="yearSell" type="radio" name="year" value="5" id="year5"><label for="year5" class="yearSellLabel"> Kvinta </label></input>
+        <input class="yearSell" type="radio" name="year" value="6" id="year6" checked><label for="year6" class="yearSellLabel"> Sexta  </label></input>
+        <input class="yearSell" type="radio" name="year" value="7" id="year7"><label for="year7" class="yearSellLabel"> Septima  </label></input>
+        <input class="yearSell" type="radio" name="year" value="8" id="year8"><label for="year8" class="yearSellLabel"> Oktáva  </label></input>
+        </div>';
 } else if (getClassByUserID($conn, $_SESSION['id'])['classYear'] == 7){
     echo '
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="">Choose a year..</option>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="4"><label> 4.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="5"><label> 5.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="6"><label> 6.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="7" checked><label> 7.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="8"><label> 8.  </label></input>';
+        <div class="yearSellContainer">
+        <input class="yearSell" type="radio" name="year" value="4" id="year4" ><label for="year4" class="yearSellLabel"> Kvarta </label></input>
+        <input class="yearSell" type="radio" name="year" value="5" id="year5"><label for="year5" class="yearSellLabel"> Kvinta </label></input>
+        <input class="yearSell" type="radio" name="year" value="6" id="year6"><label for="year6" class="yearSellLabel"> Sexta  </label></input>
+        <input class="yearSell" type="radio" name="year" value="7" id="year7" checked><label for="year7" class="yearSellLabel"> Septima  </label></input>
+        <input class="yearSell" type="radio" name="year" value="8" id="year8"><label for="year8" class="yearSellLabel"> Oktáva  </label></input>
+        </div>';
 } else if (getClassByUserID($conn, $_SESSION['id'])['classYear'] == 8){
     echo '
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="">Choose a year..</option>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="4"><label> 4.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="5"><label> 5.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="6"><label> 6.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="7"><label> 7.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="8" checked><label> 8.  </label></input>';
+        <div class="yearSellContainer">
+        <input class="yearSell" type="radio" name="year" value="4" id="year4" ><label for="year4" class="yearSellLabel"> Kvarta </label></input>
+        <input class="yearSell" type="radio" name="year" value="5" id="year5"><label for="year5" class="yearSellLabel"> Kvinta </label></input>
+        <input class="yearSell" type="radio" name="year" value="6" id="year6"><label for="year6" class="yearSellLabel"> Sexta  </label></input>
+        <input class="yearSell" type="radio" name="year" value="7" id="year7"><label for="year7" class="yearSellLabel"> Septima  </label></input>
+        <input class="yearSell" type="radio" name="year" value="8" id="year8" checked><label for="year8" class="yearSellLabel"> Oktáva  </label></input>
+        </div>';
 } else {
     echo '
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="" checked>Choose a year..</option>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="4"><label> 4.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="5"><label> 5.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="6"><label> 6.  </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="7"><label> 7. </label></input>
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="year" value="8"><label> 8.  </label></input>';
+        <div class="yearSellContainer">
+        <input class="yearSell" type="radio" name="year" value="4" id="year4"><label for="year4" class="yearSellLabel"> Kvarta </label></input>
+        <input class="yearSell" type="radio" name="year" value="5" id="year5"><label for="year5" class="yearSellLabel"> Kvinta </label></input>
+        <input class="yearSell" type="radio" name="year" value="6" id="year6"><label for="year6" class="yearSellLabel"> Sexta  </label></input>
+        <input class="yearSell" type="radio" name="year" value="7" id="year7"><label for="year7" class="yearSellLabel"> Septima  </label></input>
+        <input class="yearSell" type="radio" name="year" value="8" id="year8"><label for="year8" class="yearSellLabel"> Oktáva  </label></input>
+        </div>';
 }
 
     ?>
 
-        <input style="display:inline-block;margin-left:22%;" type="radio" name="subjectid" value="" checked="selected">Choose a subject..</option>
 
+    <div class="subjectSellContainer">
         <?php
         $subjects = getSubjects($conn);
         foreach($subjects as $subject){
-            echo "<input class='subjectSell' type='radio' name='subjectid' value='".$subject['subjectid']."'>".$subject['subjectName']."</option>";
+            echo "<input class='subjectSell' type='radio' name='subjectid' value='".$subject['subjectid']."' id='subject".$subject['subjectid']."'><label for='subject".$subject['subjectid']."' class='subjectSellLabel'>".$subject['subjectName']."</label></<input>";
         }
         ?>
+    </div>
     <h1 style="margin-left:5px;font-size:20px;">Cena</h1>
     <hr style="
     background-color: black;
@@ -123,27 +127,17 @@ if(getClassByUserID($conn, $_SESSION['id'])['classYear'] == 4){
     </select>
     <input class="ou" style="margin-top:-10px;margin-left:5%;border:2px solid black;width: 150px;"type="number" name="price" placeholder="Cena" required min="0" max="10000">
     <h1><?php
-        if (isset($_GET["error"])) {
-            if ($_GET["error"] == "emptyinput") {
-                echo "<p style='font-size:20px;margin-top:20px;justify-content: center;display: flex;color:red;'>Fill in all fields!</p>";
-            }
-            else if ($_GET["error"] == "price") {
-                echo "<p style='font-size:20px;margin-top:20px;justify-content: center;display: flex;color:red;'>Price is invalid</p>";
-            } else if ($_GET["error"] == "stmtfailed") {
-                echo "<p style='font-size:20px;margin-top:20px;justify-content: center;display: flex;color:red;'>Something went wrong, try again!</p>";
-            } else if ($_GET["error"] == "none") {
-                echo "<p style='font-size:20px;margin-top:20px;justify-content: center;display: flex;color:green;'>Your product is waiting for a customer, good job</p>";
-            }
-        }
+        include_once 'errorHandler.php';
         ?></h1>
         <br>
         <br>
         <br>
         <br>
-        <br>
+
 <hr style='padding:1px;margin-top:0px;margin-left:0px;width: 140px;background-color:black;'>
-<h3 style='font-size:18px;margin-left:150px;margin-top: -12px;'>Návrhy učebnic</h3>
-<hr style='padding:1px;margin-top:-10px;float:right;width: 85px;background-color:black;'>
+<h3 style='font-size:18px;margin-left:150px;margin-top: -12px;' class="decorationSell">Návrhy učebnic<hr style='padding:1px;margin-top:10px;float:right;width: 85px;background-color:black;'></h3>
+
+
 <?php
     if(isset($_GET['year']) && isset($_GET['subjectid'])){
         $year = $_GET['year'];
@@ -152,15 +146,16 @@ if(getClassByUserID($conn, $_SESSION['id'])['classYear'] == 4){
         $productsid = getProductsListYearSubjectid($conn, $year, $subjectid);
 
         if(empty($productsid)){
-            echo "Žádné učebnice nebyly nalezeny";
+            echo "<p class='draftSellLabel'>Žádné učebnice nebyly nalezeny</p>";
         }
 
         foreach($productsid as $productid){
             $product = getProductsListByID($conn, $productid['productslistid']);
-            echo "<input style='display:inline-block;margin-left:5px;'type='radio' name='productslistid' value='" .$product[0]['productslistid']."' required ><label> ".$product[0]['itemName']." - ". $product[0]['publishYear']."</label><br>";
+            echo "<div class='draftSellContainer'><input class='draftSell' type='radio' name='productslistid' value='" .$product[0]['productslistid']."' id='draft" .$product[0]['productslistid']."' required ><label for='draft" .$product[0]['productslistid']."'class='draftSellLabel' > ".$product[0]['itemName']." - ". $product[0]['publishYear']."</label></div><br>";
         }
     }
     ?>
+
     <button style="margin-top:10px;margin-left:25%;border-radius:25px;"class="alficek" type="submit" name="submit">Prodat</button>
 </form>
 
