b5acb55cc52626f20e67a0f4e0037a926fe79b0a
David Lausman <dauxid@gmail.com>
2023-04-10 21:15:04 +0200

    Myorders design change WIP

--- header.php
+++ header.php
@@ -10,6 +10,8 @@ include_once '../Burza/phpMailer/mailFirstLevel.php';
 <head>
     <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet" href="style.css"></link>
+    <link rel="stylesheet" href="index.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- myOrders.php
+++ myOrders.php
@@ -2,7 +2,10 @@
 include_once 'header.php';
 ?>
 
-<h2>My orders</h2>
+<div id='objednavky' class='objednavky'>
+        <div id='title-div' class='title-div'>
+            <h1 id='title-text' class='title-text'>Moje objednávky</h1>
+        </div>
 
 <a href="profile.php">Back</a>
 
@@ -15,38 +18,59 @@ if (!isset($_SESSION['id'])) {
 $userid = $_SESSION['id'];
 $products = getProductsByBuyerID($conn, $userid);
 
-include_once 'errorHandler.php';
-
 foreach ($products as $product){
-    $filename = 'Photos/books/' . $product['productslistid'] . '*';
+    $filename = 'Photos/books/'.$product['productslistid'].'*';
     $fileinfo = glob($filename);
     echo "
-<div>
-     <img style='' class='image' src='" . $fileinfo[0] . "'></img>
-<h3 style=''>" . $product['itemName'] . "</h3>
-    <br>
-    <div class='booktext'>
-        <h3 style=''>" . getRankByID($conn, $product['rankid'])['name'] . "</h3>
-        <br>
-        <h3 style=''>" . $product['price'] . "</h3>
-        <br>
-        <h3 style=''>" . getUserByID($conn, $product['userid'])['name'] . " " . getUserByID($conn, $product['userid'])['surname'] . "
-        </h3>
-          <br>
+    <div class='item-div'>
+        <div class='item-grid'>
+            <div class='img-div'>
+                <img src='" . $fileinfo[0] . "' class='img' width='75%'>
+            </div>
+
+            <div class='item-text'>
+                <div class='name-grid'>
+                    <h1 class='name'>" . $product['itemName'] . " - " . getRankByID($conn, $product['rankid'])['name'] . "</h1>
+                </div>
+                <p class='owner'>" . getUserByID($conn, $product['userid'])['name'] . " " . getUserByID($conn, $product['userid'])['surname'] . "</p>
+                <p class='reservation'>" . getStatusByID($conn, $product['statusid'])['name'] . "<br>-&gt; <!--chybí datum a místo předáni--></p>   
+                <div class='price-grid'>
+                    <p class='price'>" . $product['price'] . " Kč</p>
+                </div>
+            </div>
+        </div>
+
+        <div class='buttons-div'>
+                
+   <!--      
             <h3 style=''>
             " . date("d-m-Y", strtotime($product['buyTime']))  . "
-</h3>
-          <br>
-            <h3 style=''>" . getStatusByID($conn, $product['statusid'])['name'] . "</h3>
-        <br>
-        <input type='number' name='productid' hidden='hidden' value=" . $product['id'] . ">
-        
+</h3> ##datum objednání k nicemu
+        <input type='number' name='productid' hidden='hidden' value=" . $product['id'] . "> ##co tohle sakra dělá?!
+        -->
+
         ";
    if ($product['statusid'] == 2) {
-        echo"<button class='alficek2'><a class='black' href='includes/MY.inc.php?issue=markassold&id=".$product['id']."&return=myOrders.php'>Mark as bought</a></button>";
-}
-echo "
-        <button class='alficek2'><a  class='black' href='contact.php?id=".$product['id']."&return=myOrders.php'>Contact</a>";
+        echo"<button class='kontakt-button'><a  class='kontakt-button-text' href='contact.php?id=".$product['id']."&return=myOrders.php'>Kontakt <i class='material-icons' style='font-size: 3vw;'>chat_bubble_outline</i>";
+
+       foreach (getMessagesByProductID($conn, $product['id']) as $message) {
+           if ($message['isViewed'] == 0  && $message['recieverid'] == $_SESSION['id']){
+               echo "<i class='material-icons' style='font-size: 3vw;'>sms_failed</i>"; ##oznameni ze prisla zprava
+               break;
+           }
+       }
+        echo "</a></button>
+        <button class='cancel-button'><a class='cancel-button-text' >Zrušit rezervaci</a></button>
+    </div>
+</div>
+        ";
+    }else{
+        echo"<i class='material-icons' style='font-size: 3vw;'>chat_bubble_outline</i></a></button>
+        <button class='cancel-button'><a class='cancel-button-text' >Zrušit rezervaci</a></button>
+    </div>
+</div>";
+
+##link to cancel product - href='includes/MY.inc.php?issue=markassold&id=".$product['id']."&return=myOrders.php'
 
        foreach (getMessagesByProductID($conn, $product['id']) as $message) {
            if ($message['isViewed'] == 0  && $message['recieverid'] == $_SESSION['id']){
@@ -55,9 +79,9 @@ echo "
            }
        }
        echo "</button>
-        </div>
-  </div>
-        ";
+</div>
+  </div>";
+    }
 
 }
 ?>
